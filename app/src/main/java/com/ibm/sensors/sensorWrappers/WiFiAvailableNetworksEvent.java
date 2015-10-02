a20dbe1b48e3cbdf70fe7ca93770dbcce87a122f
flow.r3direction@gmail.com <flow.r3direction@gmail.com>
2015-10-02 04:48:39 -0700

    added stuff wifi

---
+++ app/src/main/java/com/ibm/sensors/sensorWrappers/WiFiAvailableNetworksEvent.java
@@ -0,0 +1,48 @@
+package com.ibm.sensors.sensorWrappers;
+
+import android.net.wifi.ScanResult;
+import com.google.gson.Gson;
+import com.ibm.sensors.EventWrappers.EventWrapper;
+import com.ibm.sensors.core.SensorAndRuleFactory;
+
+
+import java.util.List;
+
+/**
+ * Created by nexus on 04/09/2015.
+ */
+public class WiFiAvailableNetworksEvent implements EventWrapper {
+
+    private List<ScanResult> mResults;
+
+    public WiFiAvailableNetworksEvent(List<ScanResult> results){
+        this.mResults = results;
+    }
+
+
+
+    @Override
+    public String toJson(Gson gson) {
+        return gson.toJson(this);
+    }
+
+    @Override
+    public int getEventType() {
+        return SensorAndRuleFactory.TYPE_AVAILABLE_WIFI_NETWORKS;
+    }
+
+    @Override
+    public SensorWrapper getSensor() {
+        return null;
+    }
+
+    @Override
+    public Object getData() {
+        return null;
+    }
+
+    @Override
+    public long getTime() {
+        return 0;
+    }
+}
