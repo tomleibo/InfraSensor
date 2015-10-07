25e48debc6a82fa7c3ed8b76df5122f748324221
flow.r3direction@gmail.com <flow.r3direction@gmail.com>
2015-10-07 10:00:32 -0700

    added low pass modifier

--- app/src/main/java/com/ibm/sensors/modifiers/FloatAccumilator.java
+++ app/src/main/java/com/ibm/sensors/modifiers/FloatAccumilator.java
@@ -1,14 +1,35 @@
 package com.ibm.sensors.modifiers;
 
+import com.ibm.sensors.EventWrappers.EventWrapper;
+
+import java.util.ArrayList;
+
 /**
  * Created by nexus on 05/10/2015.
  */
+//TODO: fix this to more than 3
 public class FloatAccumilator extends AbstractArrayAccumilator<Float>{
+	public FloatAccumilator(){
+		this.mData=new ArrayList<Float>();
+		mData.add(new Float(0));
+		mData.add(new Float(0));
+		mData.add(new Float(0));
+	}
 
 	@Override
 	protected Float method(Float oldData, Float newData) {
-		if (null==oldData) return newData;
-		if (null==newData) return oldData;
-		return  oldData+newData;
+		Float data= null;
+		if (null==oldData) data = newData;
+		if (null==newData) data =  oldData;
+		if (null!=oldData && null!= newData){
+			return  oldData+newData;
+		}
+		return data;
+	}
+	@Override
+	public void aggregate(EventWrapper<Float[]> input) {
+		for (int i=0;i<input.getData().length;i++){
+			this.mData.set(i,method(this.mData.get(i),input.getData()[i]));
+		}
 	}
 }
